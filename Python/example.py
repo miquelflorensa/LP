e5bb65a585b0fb7a66b18cae65e58370bccd8265
miquelflorensa <91897595+miquelflorensa@users.noreply.github.com>
2022-01-03 21:21:30 +0100

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -4,12 +4,17 @@ from ExprParser import ExprParser
 from TreeVisitor import TreeVisitor
 import sys
 #input_stream = InputStream(input('? '))
-input_stream = FileStream(sys.argv[1])
+input_stream = FileStream(sys.argv[1],encoding='utf-8')
 lexer = ExprLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 parser = ExprParser(token_stream)
 tree = parser.root()
 
+if len(sys.argv) == 3:
+    visitor = TreeVisitor(sys.argv[2])
+elif len(sys.argv) > 3:
+    visitor = TreeVisitor(sys.argv[2], [param for param in sys.argv[3:]])
+else:
+    visitor = TreeVisitor()
 
-visitor = TreeVisitor()
 visitor.visit(tree)
